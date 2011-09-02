f50cca92accb78cd28a70f674c4d075bfc581a7b
Sina Khakbaz Heshmati <sina@khakbaz.com>
2011-09-03 01:09:43 +0200

    Improves the script that deletes auxiliary files and orders files matching the #*# pattern to be deleted a long with other auxiliary files

--- neille/utils/bash/neille-rm-aux-files.bash
+++ neille/utils/bash/neille-rm-aux-files.bash
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-find . -name 'compiled' -type d -exec rm -r {} ';'
-find . -name '*~' -o -name '.DS_Store' -type f | xargs rm
+echo
+echo
+echo
+echo
+
+function neille-find-compiled-dirs 
+{
+  find . -name 'compiled' -type d
+}
+
+function neille-find-aux-files 
+{
+  find . -name '*~' -o -name '.DS_Store' -o -name '#*#' -type f
+}
+
+neille-find-compiled-dir | xargs rm -r
+
+neille-find-aux-files | xargs rm
+
