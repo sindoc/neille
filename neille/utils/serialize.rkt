6f95cc5906d59613b52eccfe3cd592ab71aeb770
Sina Khakbaz Heshmati <sina@khakbaz.com>
2011-09-03 02:01:26 +0200

    Adapts the formatting of the source code in /neille/utils/serialize.rkt

--- neille/utils/serialize.rkt
+++ neille/utils/serialize.rkt
@@ -1,16 +1,33 @@
 #lang racket
 
-(provide (all-defined-out))
+
+
+(provide 
+ 
+ (all-defined-out))
+
+
 
 (define storage-file-path "neille-storage")
 
+
+
 (define-struct ws-player
+  
   (name squads)
+  
   #:prefab)
+
+
 
 (define-struct ws-squad
+  
   (hero units)
+  
   #:prefab)
 
+
+
 (define (serialize- any)
+  
   (send any externalize))
