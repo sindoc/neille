53904667585d2652dd2bac8d76a94f6ff97bbf55
Sina Khakbaz Heshmati <sina@khakbaz.com>
2011-09-06 15:41:46 +0200

    Implements a simple procedure to assert conditions

--- neille/utils/base.rkt
+++ neille/utils/base.rkt
@@ -1,21 +1,51 @@
 #lang racket
 
-(provide (all-defined-out))
+
+
+(provide 
+ 
+ (all-defined-out))
+
+
 
 (define (show #:sep [sep " "] . args)
+  
   (for-each
+   
    (lambda (arg)
+     
      (display arg)
+     
      (display sep))
+   
    args)
+  
   (newline)
+  
   void)
 
+
+
 (define (vector-map! vect proc)
+  
   (define last (- (vector-length vect) 1))
+  
   (let loop
+    
     ((i 0))
+    
     (vector-set! vect i (proc i (vector-ref vect i)))
+    
     (when (< i last)
+      
       (loop (+ i 1))))
+  
   vect)
+
+
+
+(define (assert condition msg context)
+  
+  (when (false? condition)
+      
+    (error msg context)))
